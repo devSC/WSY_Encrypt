aba974d2e761ec59bc5efd8b844e1f6d20cc62e0
袁仕崇 <xiaochong2154@163.com>
2015-04-21 00:03:31 +0800

    COMMINT

---
+++ TestEncrypt/My Today Extension/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  My Today Extension
+//
+//  Created by 袁仕崇 on 15/4/5.
+//  Copyright (c) 2015年 WoWSai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
--- TestEncrypt/TestEncrypt/ViewController.m
+++ TestEncrypt/TestEncrypt/ViewController.m
@@ -20,6 +20,7 @@ static NSString *skgString = @"http://mov1.shougongke.com/x/22.sgk";
 static NSString *mp4String = @"http://mov1.shougongke.com/x/21.mp4";
 
 static NSString *originKey = @"da2514efeb1ad217140454taskwn49c1283062467080280c8c742869ffbeb1669618a4a308475161474798bbd02d3810955817359843taskwn4a77605eaa89f93a6e9954e020b10d6be129349348ab1166fc79ecd13ed837a829f0073610189643dde15fb4688e6d";
+static char *originKeyC = "da2514efeb1ad217140454taskwn49c1283062467080280c8c742869ffbeb1669618a4a308475161474798bbd02d3810955817359843taskwn4a77605eaa89f93a6e9954e020b10d6be129349348ab1166fc79ecd13ed837a829f0073610189643dde15fb4688e6d";
 
 @implementation ViewController
 
@@ -30,6 +31,8 @@ static NSString *originKey = @"da2514efeb1ad217140454taskwn49c1283062467080280c8
     
 //    self.view.backgroundColor = [UIColor cyanColor];
     
+//    [self test];
+//    return;
     NSError *error = nil;
     
     [self downloadTheFile];
@@ -55,23 +58,38 @@ static NSString *originKey = @"da2514efeb1ad217140454taskwn49c1283062467080280c8
 
             //5. 把 key0fix 逐字节与 key1 进行 XOR 计算，得到 trueKey；
 //               Byte *trueKey = [self trueKeyWithI ndex:headByte[5]];
-               Byte *trueKey = [self trueKeyWithHeader:headByte];
+            Byte *trueKey = [self trueKeyWithHeader:headByte];
+//            Byte *trueKey = [self trueKeyWithHead:headByte];
+            NSLog(@"trueKey: %s\n\n\n", trueKey);
+
             /* 6. 继续读取需要解密的文件，并打开输出文件，
              如果是前 1024 个字节，与 trueKey 的对应位置进行 XOR 计算，并输出；
              否则，直接输出。
              */
-            NSLog(@"%llu", [fileHandle offsetInFile]);
+//            NSLog(@"%llu", [fileHandle offsetInFile]);
             [fileHandle seekToFileOffset:[fileHandle offsetInFile]];
-            NSData *headData = [fileHandle readDataOfLength:1024];
-            Byte *newHeadByte = (Byte *)headData.bytes;
-            for (int i = 0; i < 1024; i++) {
-                newHeadByte[i] = trueKey[i] ^ newHeadByte[i];
+            NSMutableData *headData = [[fileHandle readDataOfLength:1024] mutableCopy];
+            Byte *newHeadByte = (Byte *)headData.mutableBytes;
+            NSLog(@"newHeadByte: %s", newHeadByte);
+            Byte headerFile[] = {};
+            for (int i = 0; i < headData.length; i++) {
+                char trueChar = trueKey[i];
+////                char headerChar = newHeadByte[i];
+////                headerFile[i] = trueChar ^ headerChar;
+                NSLog(@"i = %d %hhu %hhu", i, trueKey[i], newHeadByte[i]);
+                newHeadByte[i] = (Byte)(newHeadByte[i] ^ trueChar);
+                NSLog(@"*****i = %d %hhu %hhu", i, trueKey[i], newHeadByte[i]);
+//                newHeadByte[i] ^= trueKey[i];
             }
+//            NSData *data = [self obfuscate:headData withKey:trueKey];
+            
+            NSLog(@"newHeadByte: %s", newHeadByte);
+
             [self chectFileExistWithFilePath:movFilePath];
 
             [[NSData dataWithBytes:newHeadByte length:1024] writeToFile:movFilePath atomically:YES];;
 //            [fileHandle offsetInFile];
-            NSLog(@"%llu", [fileHandle offsetInFile]);
+//            NSLog(@"%llu", [fileHandle offsetInFile]);
 
             [fileHandle seekToFileOffset:[fileHandle offsetInFile]];
             [[fileHandle readDataToEndOfFile] writeToFile:movFilePath atomically:YES];
@@ -112,19 +130,50 @@ static NSString *originKey = @"da2514efeb1ad217140454taskwn49c1283062467080280c8
     while (keySub.length < 1024) {
         keySub =[keySub stringByAppendingString:keySub];
     }
-    NSData *ksData = [keySub dataUsingEncoding: NSUTF8StringEncoding];
+    NSLog(@"keySub: %@ \n\n\n", keySub);
+    NSData *ksData = [keySub dataUsingEncoding: NSMacOSRomanStringEncoding];
     Byte *ksBytes = (Byte *)[ksData bytes];
-//        Byte *ksBytes = (Byte *)[keySub  hexToBytes].bytes;
-//    Byte *bytes = keySub.
+    NSLog(@"ksBytes: %s \n\n\n", ksBytes);
+    char key = header[5];
+    for (int i = 0; i < 1024; i++) {
+        //进行异或运算
+        ksBytes[i] =(Byte)(ksBytes[i] ^ key);
+    }
+    NSLog(@"ksBytes: %s", ksBytes);
+    return ksBytes;
+}
+
+- (Byte *)trueKeyWithHead: (Byte *)header
+{
+    //4. 从 offset 为 key1 处，截取解密密钥 key0，得到 key0sub；扩展长度到 1024 字节，以下称为 key0fix；
+//    Byte *key =
+    char *byte;
+//    for (int i = header[5], j = 0; i < strlen(originKeyC); i ++, j++) {
+//        byte[j] = originKeyC[i];
+//    }
+    NSString *keySub = [originKey substringFromIndex:header[5]];
+    while (keySub.length < 1024) {
+        keySub =[keySub stringByAppendingString:keySub];
+    }
+
+    byte = [keySub cString];
+    while (strlen(byte) < 1024) {
+        byte = strcat(byte, byte);
+    }
+    //char -> byte
+    Byte *ksByte;
+    for (int i=0;i<strlen(byte);i++){
+//        printf("%02X",byte[i]&0xff);//16进制
+        ksByte[i] = byte[i]^0xff;
+    }
     
     for (int i = 0; i < 1024; i++) {
         //进行异或运算
-        ksBytes[i] =(ksBytes[i]^header[5]);
+        ksByte[i] =(ksByte[i]^header[5]);
     }
-//    NSLog(@"%@", keySub);
-    //ksBytes	Byte *	"|~z}z\x7f|\x7f|rs))/{y/xsz{r~~sz|x~rs\x7fx?*8 <%\x7f*||}{~.**sr-rx*}.rr~\x7f.{y{)z{/}).zyrx\x7frx\x7fs*)zz}}-(|r.(/zx./sx|*syr-{{|x}z{zsr}\x7fx//.z~-)\x7f}ss.}/|~z}z\x7f|\x7f|rs))/{y/xsz{r~~sz|x~rs\x7fx?*8 <%\x7f*||}{~.**sr-rx*}.rr~\x7f.{y{)z{/}).zyrx\x7frx\x7fs*)zz}}-(|r.(/zx./sx|*syr-{{|x}z{zsr}\x7fx//.z~-)\x7f}ss.}/|~z}z\x7f|\x7f|rs))/{y/xsz{r~~sz|x~rs\x7fx?*8 <%\x7f*||}{~.**sr-rx*}.rr~\x7f.{y{)z{/}).zyrx\x7frx\x7fs*)zz}}-(|r.(/zx./sx|*syr-{{|x}z{zsr}\x7fx//.z~-)\x7f}ss.}/|~z}z\x7f|\x7f|rs))/{y/xsz{r~~sz|x~rs\x7fx?*8 <%\x7f*||}{~.**sr-rx*}.rr~\x7f.{y{)z{/}).zyrx\x7frx\x7fs*)zz}}-(|r.(/zx./sx|*syr-{{|x}z{zsr}\x7fx//.z~-)\x7f}ss.}/|~z}z\x7f|\x7f|rs))/{y/xsz{r~~sz|x~rs\x7fx?*8 <%\x7f*||}{~.**sr-rx*}.rr~\x7f.{y{)z{/}).zyrx\x7frx\x7fs*)zz}}-(|r.(/zx./sx|*syr-{{|x}z{zsr}\x7fx//.z~-)\x7f}ss.}/|~z}z\x7f|\x7f|rs))/{y/xsz{r~~sz|x~rs\x7fx?*8 <%\x7f*||}{~.**sr-rx*}.rr~\x7f.{y{)z{/}).zyrx\x7frx\x7fs*)zz}}-(|r.(/zx./sx|*syr-{{|x}z{zsr}\x7fx//.z~-)\x7f}ss.}/|~z}z\x7f|\x7f|rs))/{y/xsz{r~~sz|x~rs\x7fx?*8 <%\x7f*||}{~.**sr-rx*}.rr~\x7f.{y{)z{/}).zyrx\x7frx\x7fs*)zz}}-(|r.(/zx./sx|*syr-{{|x}z{zsr}\x7fx//.z~-)\x7f}ss.}/|~z}z\x7f|\x7f|rs))/{y/xsz{r~~sz|x~rs\x7fx?*8 <%\x7f*||}{~.**sr-rx*}.rr~\x7f.{y{)z{/}).zyrx\x7frx\x7fs*)zz}}-(|r.(..."	0x14ab6a00
-    return ksBytes;
+    return ksByte;
 }
+
 - (BOOL)checkKeyVersionWithByte: (Byte *)byte{
     return byte[3] == 0x31 ? YES : NO;
 }
@@ -180,6 +229,75 @@ static NSString *originKey = @"da2514efeb1ad217140454taskwn49c1283062467080280c8
     }
     
 }
+//- (NSData *)obfuscate:(NSData *)data withKey:(NSString *)key
+//{
+//    NSMutableData *result = [data mutableCopy];
+//    
+//    
+//    // Get pointer to data to obfuscate
+//    char *dataPtr = (char *) [result mutableBytes];
+//    
+//    // Get pointer to key data
+//    char *keyData = (char *) [[key dataUsingEncoding:NSUTF8StringEncoding] bytes];
+//    
+//    // Points to each char in sequence in the key
+//    char *keyPtr = keyData;
+//    int keyIndex = 0;
+//    
+//    // For each character in data, xor with current value in key
+//    for (int x = 0; x < [data length]; x++)
+//    {
+//        // Replace current character in data with
+//        // current character xor'd with current key value.
+//        // Bump each pointer to the next character
+////        *dataPtr = *dataPtr++ ^ *keyPtr++;
+//        *dataPtr = *dataPtr ^ *keyPtr;
+//        dataPtr++;
+//        keyPtr++;
+//        // If at end of key data, reset count and
+//        // set key pointer back to start of key value
+//        if (++keyIndex == [key length])
+//            keyIndex = 0, keyPtr = keyData;
+//    }
+//    
+//    return result;
+//}
+
+- (NSData *)obfuscate:(NSData *)data withKey:(Byte *)key
+{
+    NSMutableData *result = [data mutableCopy];
+    
+    
+    // Get pointer to data to obfuscate
+    char *dataPtr = (char *) [result mutableBytes];
+    
+    // Get pointer to key data
+//    char *keyData = (char *) [[key dataUsingEncoding:NSUTF8StringEncoding] bytes];
+    char *keyData = (char *) key;
+    
+    // Points to each char in sequence in the key
+    char *keyPtr = keyData;
+    int keyIndex = 0;
+    
+    // For each character in data, xor with current value in key
+    for (int x = 0; x < [data length]; x++)
+    {
+        // Replace current character in data with
+        // current character xor'd with current key value.
+        // Bump each pointer to the next character
+        //        *dataPtr = *dataPtr++ ^ *keyPtr++;
+        *dataPtr = *dataPtr ^ *keyPtr;
+        dataPtr++;
+        keyPtr++;
+        // If at end of key data, reset count and
+        // set key pointer back to start of key value
+        if (++keyIndex == strlen(keyData))
+            keyIndex = 0, keyPtr = keyData;
+    }
+    
+    return result;
+}
+
 
 
 /*
